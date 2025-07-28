75247ea7a2210433d448fed1974c7cd3a10863ca
CharlyCRM <charlycrm@hotmail.com>
2025-07-28 10:31:14 +0200

    feat: creacion fichero main

---
+++ biblioteca_inteligente/main.py
@@ -0,0 +1,10 @@
+from libro import Libro
+from usuario import Usuario
+
+
+if __name__ == "__main__":
+    # Creación de libros
+    libro1 = Libro("Programación Python", "Brian Jason", 34562, 2024, "Informática", True)
+
+    # Creación de Usuario
+    usuario1 =
