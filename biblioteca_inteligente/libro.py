b25003e5670f06c4ca9a7125cf16899e5fd89642
CharlyCRM <charlycrm@hotmail.com>
2025-08-24 23:45:46 +0200

    feat: agrego metodo para comprobar si un libro es nuevo

--- biblioteca_inteligente/libro.py
+++ biblioteca_inteligente/libro.py
@@ -1,7 +1,8 @@
 # Clase Libro
+from datetime import date
 
 class Libro:
-    def __init__(self, titulo:str, autor:str, isbn:int, año:int, genero:str, disponible:bool):
+    def __init__(self, titulo:str, autor:str, isbn:int, año:date, genero:str, disponible:bool):
         self.titulo = titulo
         self.autor = autor
         self.isbn = isbn
@@ -10,10 +11,12 @@ class Libro:
         self.disponible = disponible
 
     def marcar_como_prestado(self) -> None:
+        "Marca un libro como prestado cambiando el atributo dispinible a false"
         self.disponible = False
         print(f"📙 {self.titulo} ha sido prestado correctamente")
 
     def marcar_como_disponible(self) -> None:
+        "Marca un libro como disponible cambiando el atributo dispinible a tru"
         self.disponible = True
         print(f"📙 {self.titulo} ha sido devuelto correctamente")
 
@@ -26,3 +29,14 @@ class Libro:
             f"Género: {self.genero}"
             f"Disponibilidad: {self.disponible}\n"
     )
+
+    def es_nuevo(self) -> None:
+        "Comprueba si el libro es nuevo (< 2 años)"
+        if self.año < date.today():
+            print(f"📙 {self.titulo} es un libro antígüo")
+        else:
+            print(f"📙 {self.titulo} es un libro nuevo")
+
+
+libro1 = Libro("100 años de Soledad", "Adolfo Bequer", 23489, date(1997, 10, 30), "Poesia", True)
+libro1.es_nuevo()
